4ef816f6ccc1477a93b871fb590ca43673b93ef4
tishachawla-jg <76087547+tishachawla-jg@users.noreply.github.com>
2021-02-25 21:30:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const body = document.body;
+const collapseBtn = document.querySelector(".admin-menu button");
+const collapsedClass = "collapsed";
+ 
+collapseBtn.addEventListener("click", function() {
+  this.getAttribute("aria-expanded") == "true"
+    ? this.setAttribute("aria-expanded", "false")
+    : this.setAttribute("aria-expanded", "true");
+  this.getAttribute("aria-label") == "collapse menu"
+    ? this.setAttribute("aria-label", "expand menu")
+    : this.setAttribute("aria-label", "collapse menu");
+  body.classList.toggle(collapsedClass);
+});
+const body = document.body;
+const menuLinks = document.querySelectorAll(".admin-menu a");
+const collapsedClass = "collapsed";
+ 
+for (const link of menuLinks) {
+  link.addEventListener("mouseenter", function() {
+    body.classList.contains(collapsedClass) &&
+    window.matchMedia("(min-width: 768px)").matches
+      ? this.setAttribute("title", this.textContent)
+      : this.removeAttribute("title");
+  });
+}
